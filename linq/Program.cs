b2793d3517ab6ee30daa3ae892cb1ef0a488a93d
Zafuzi <zacharyfoutz@gmail.com>
2026-06-01 13:40:25 -0700

    more linq

--- linq/Program.cs
+++ linq/Program.cs
@@ -1,12 +1,20 @@
-﻿List<int> scores = [97, 92, 81, 60, 10, 20, 30, 40, 50, 88, 89, 73, 100, 99, 97, 93, 82];
+﻿List<int> scores = [3, 45, 82, 97, 92, 100, 81, 60];
 
 IEnumerable<string> scoreQuery =
 	from score in scores
-	where score > 70
-	orderby score ascending
-	select $"The score is {score}";
+	where score > 80
+	orderby score descending
+	select $"Found score: {score}";
 
-foreach (string s in scoreQuery)
+/*
+var scoreQuery = scores
+	.Where(score => score > 80)
+	.OrderByDescending(s => s);
+*/
+
+List<string> myScores = scoreQuery.ToList();
+
+foreach (var s in myScores)
 {
 	Console.WriteLine(s);
 }
