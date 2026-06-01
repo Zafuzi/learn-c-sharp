90b37a87d9b3f99669e14d379bf0698168da7575
Zafuzi <zacharyfoutz@gmail.com>
2026-06-01 09:10:51 -0700

    finished hello world

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, Zach!");
+﻿string firstName = "Noah";
+string secondName = "Sbonga";
+string friends = $"My friends are {firstName.Trim()} and {secondName.Trim()}.";
+string otherFriend = friends.Replace("Noah", "Joe"); 
+
+Console.WriteLine(friends);
+Console.WriteLine(friends.Length);
+Console.WriteLine(otherFriend.Contains(firstName));
