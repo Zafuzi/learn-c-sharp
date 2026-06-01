0baa9661692a2565f7561233f3dde461cb512768
Zafuzi <zacharyfoutz@gmail.com>
2026-06-01 10:50:45 -0700

    update branching

--- branching/Program.cs
+++ branching/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int a = 5;
+int b = 6;
+
+int c = a - b;
+
+int check = 10;
+bool myTest = c > check;
+
+if (myTest)
+{
+	Console.WriteLine($"The answer is greater than {check} | c = {c}");
+}
+else
+{
+	Console.WriteLine($"The answer is less than {check} | c = {c}");
+}
